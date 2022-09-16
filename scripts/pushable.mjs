5b6052ed9f0cdae02fb2df90a485788dbacb2263
oOve <oOve@users.noreply.github.com>
2022-09-16 16:02:37 +0200

    Pushable now V10 compatible

--- scripts/pushable.mjs
+++ scripts/pushable.mjs
@@ -11,12 +11,12 @@
  ░                 ░              
  */
 
- const MOD_NAME = "pushable";
+const MOD_NAME = "pushable";
+const LAMBDA = 5;
 
 function Lang(k){
   return game.i18n.localize("PUSHABLE."+k);
 }
-
 
 let pushable_socket;
 Hooks.once("socketlib.ready", () => {
@@ -31,27 +31,45 @@ function doMoveAsGM(updates){
 }
 
 
+/**
+ * Display a text above a token
+ * @param {*} token A token object
+ * @param {String} text The text to display above the token
+ */
+ function scrollText(token, text){
+  let config = {
+    x: token.x,
+    y: token.y,
+    text: text,
+    anchor: CONST.TEXT_ANCHOR_POINTS.TOP, 
+    fill:   "#FFFFFF", 
+    stroke: "#FFFFFF"
+  }
+  canvas.interface.createScrollingText(token, text, config);
+}
+
 function rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2) {
   // Check x and y for overlap
   return !(x2 > w1 + x1 || x1 > w2 + x2 || y2 > h1 + y1 || y1 > h2 + y2);  
 }
 
 function isPushable(token){
-  return (token.data.flags.pushable)&&(token.data.flags.pushable.isPushable);
+  return token.document?.flags.pushable?.isPushable ||
+         token.flags?.pushable?.isPushable; 
 }
 
 // Return list of tokens overlapping 'token'
 function find_collisions(token){
-  let x1=token.data.x+1;
-  let y1=token.data.y+1;
-  let w1=token.hitArea.x + token.hitArea.width-2;
-  let h1=token.hitArea.y + token.hitArea.height-2;
+  let x1=token.x+LAMBDA;
+  let y1=token.y+LAMBDA;
+  let w1=token.hitArea.x + token.hitArea.width -(LAMBDA*2);
+  let h1=token.hitArea.y + token.hitArea.height-(LAMBDA*2);
   let collisions = [];
 
   for (let tok of canvas.tokens.placeables){
     if (tok.id != token.id){
-        let x2=tok.data.x;
-        let y2=tok.data.y;
+        let x2=tok.x;
+        let y2=tok.y;
         let w2=tok.hitArea.x + tok.hitArea.width;
         let h2=tok.hitArea.y + tok.hitArea.height;
         
@@ -65,29 +83,21 @@ function find_collisions(token){
 
 function duplicate_tk(token){
   return {  
-          data:
-          {
-            id:token.id,
-            x: token.data.x, 
-            y: token.data.y,
-            flags: {
-              pushable: {
-                isPushable: isPushable(token)
-              }
-            }
-          },
-          hitArea: token.hitArea,
           id:token.id,
+          x: token.x, 
+          y: token.y,
+          flags: { pushable:{isPushable: isPushable(token)}},            
+          hitArea: token.hitArea,
           _id:token.id
         };
 }
 
 // Does the centerpoint of 'token' collide with wall if moved along 'direction'
 function collides_with_wall(token, direction){
-  let cx = token.data.x + (token.hitArea.width/2);
-  let cy = token.data.y + (token.hitArea.height/2);
+  let cx = token.x + (token.hitArea.width/2);
+  let cy = token.y + (token.hitArea.height/2);
   let ray = new Ray({x:cx, y:cy}, {x:cx+direction.x, y:cy+direction.y});
-  return canvas.walls.checkCollision(ray);
+  return canvas.walls.checkCollision(ray, {type:'move',mode:'any'});
 }
 
 
@@ -102,14 +112,15 @@ function candidate_move(token, direction, updates, depth){
   // Exit early to avoid doing sqrt
   if (colls.length==0){return result;}
   
+  
   let len = Math.sqrt(direction.x**2+direction.y**2);
   let dir = {x:direction.x/len, y:direction.y/len};
 
   let wePushable = isPushable(token);
 
   for (let coll_obj of colls){
-    let nx=coll_obj.data.x;
-    let ny=coll_obj.data.y;
+    let nx=coll_obj.x;
+    let ny=coll_obj.y;
   
     let collPushable = isPushable(coll_obj);
     
@@ -131,13 +142,13 @@ function candidate_move(token, direction, updates, depth){
     }
     
     if (direction.x){ // dir.x != 0
-      //                     positive                            :  negative
-      nx = (direction.x>0)? (token.data.x  + token.hitArea.width): (nx = token.data.x - coll_obj.hitArea.width);
+      //                     positive                        :  negative
+      nx = (direction.x>0)?( token.x + token.hitArea.width  ):( nx = token.x - coll_obj.hitArea.width);
     }
-    if (direction.y){ //     Positive                            :   Negative
-      ny = (direction.y>0)?(token.data.y + token.hitArea.height) : (token.data.y - coll_obj.hitArea.height);
+    if (direction.y){ //     Positive                        :   Negative
+      ny = (direction.y>0)?( token.y + token.hitArea.height ):( token.y - coll_obj.hitArea.height);
     }
-    let new_dir = {x: nx-coll_obj.data.x, y: ny-coll_obj.data.y};
+    let new_dir = {x: nx-coll_obj.x, y: ny-coll_obj.y};
     
     // Does this "new_dir" take coll_obj through a wall?
     if (collides_with_wall(coll_obj, new_dir)){
@@ -153,8 +164,8 @@ function candidate_move(token, direction, updates, depth){
     }
     
     let candidate_token = duplicate_tk(coll_obj);
-    candidate_token.data.x += new_dir.x;
-    candidate_token.data.y += new_dir.y;
+    candidate_token.x += new_dir.x;
+    candidate_token.y += new_dir.y;
     let res = candidate_move(candidate_token, new_dir, updates, depth+1);   
     result.valid &= res.valid;
     if (!res.valid){
@@ -167,10 +178,10 @@ function candidate_move(token, direction, updates, depth){
 // Returns a token overlapping point 'p', return null if none exists.
 function tokenAtPoint(p){
   for (let tok of canvas.tokens.placeables){
-    if (p.x > tok.data.x && 
-        p.x < tok.data.x+tok.hitArea.width &&
-        p.y > tok.data.y &&
-        p.y < tok.data.y+tok.hitArea.height){
+    if (p.x > tok.x && 
+        p.x < tok.x+tok.hitArea.width &&
+        p.y > tok.y &&
+        p.y < tok.y+tok.hitArea.height){
           return tok;
         }
   }
@@ -183,46 +194,36 @@ function checkPull(token, direction, updates){
   let l = Math.sqrt( direction.x**2 + direction.y**2 );
   // nv is the normalized direction vector
   let nv = {x:direction.x/l, y:direction.y/l};
-  let center = {x: token.data.x + token.hitArea.width/2, y: token.data.y + token.hitArea.height/2};
-  let pull_from = {x: center.x - token.hitArea.width*nv.x,
+  let center = {x: token.x + token.hitArea.width/2, y: token.y + token.hitArea.height/2};
+  let pull_from = {x: center.x - token.hitArea.width *nv.x,
                    y: center.y - token.hitArea.height*nv.y };
-  let ray = new Ray(pull_from, center);  
-  if (canvas.walls.checkCollision(ray)){
+  let ray = new Ray(pull_from, center);
+  if (canvas.walls.checkCollision(ray, {type:'move',mode:'any'})){
     return {valid:false, reason: "CantPull"};
   }
   
   // We also need to check if there are other tokens, than the "puller" at the destination
   let colls = find_collisions(token);
-  //console.warn("Pulling", token.name, "at", colls);
   if (colls.length && game.settings.get(MOD_NAME, 'collideWithNonPushables')){
     return { valid:false, reason: "CantPullEntity" }
   }
-
   
   let pulle = tokenAtPoint(pull_from);  
   if (pulle){
     if (pulle.document.getFlag(MOD_NAME, 'isPullable')){
-      updates.push({id:pulle.id, x: pulle.data.x+direction.x, y: pulle.data.y+direction.y, _id:pulle.id});
+      updates.push({id:pulle.id, x: pulle.x+direction.x, y: pulle.y+direction.y, _id:pulle.id});
     }
     else{
       return {valid: false, reason: "CantPull"};
     }
   }
-
   return {valid: true};
 }
 
 
 function showHint(token, hint, isError=true){
   if (game.settings.get(MOD_NAME, "showHints")){
-    token.hud.createScrollingText(hint, {
-      //duration: 5000, 
-      anchor: CONST.TEXT_ANCHOR_POINTS.TOP, 
-      //direction: CONST.TEXT_ANCHOR_POINTS.LEFT, 
-      fill: (isError)?"#FF1111":"#FFFFFF", 
-      stroke: (isError)?"#FF1111":"#FFFFFF",
-      //fontSize: 50
-    });
+    scrollText(token, hint);
   }
 }
 function overLimit( updates ){
@@ -238,16 +239,17 @@ Hooks.on('preUpdateToken', (token, change, options, user_id)=>{
   if (hasProperty(options, 'pushable_triggered')){ return true; }  // We don't need to pre-evaluate already approved moves.
   if (!hasProperty(change,'x')&&!(hasProperty(change, 'y'))){return true;}
 
-  let nx = (hasProperty(change, 'x'))?(change.x):(token.data.x);
-  let ny = (hasProperty(change, 'y'))?(change.y):(token.data.y);
-  let direction = {x:nx-token.data.x, y: ny-token.data.y};
+  let nx = (hasProperty(change, 'x'))?(change.x):(token.x);
+  let ny = (hasProperty(change, 'y'))?(change.y):(token.y);
+  let direction = {x:nx-token.x, y: ny-token.y};
   let tok=canvas.tokens.get(token.id);
-  
-  
+    
   let token_after_move = duplicate_tk(tok);
-  token_after_move.data.x = nx;
-  token_after_move.data.y = ny;
+  token_after_move.x = nx;
+  token_after_move.y = ny;
   let res = {valid:true};
+
+  console.log(token, change, options, token_after_move);  
 
   let updates = [];
   if (game.settings.get("pushable", "pull")){
@@ -256,7 +258,7 @@ Hooks.on('preUpdateToken', (token, change, options, user_id)=>{
     for (let k of pk){
       pulling = pulling || keyboard.downKeys.has(k.key);
     }
-    if (pulling){      
+    if (pulling){
       let res = checkPull(tok, direction, updates);
       if (!res.valid ){
         showHint(tok, Lang(res.reason));
@@ -316,7 +318,7 @@ Hooks.once("init", () => {
     hint: Lang("PullKeyHint"),
     editable: [
       {
-        key: Lang("PullKeyDefault")
+        key: 'KeyP'
       }
     ],
     restricted: false,
